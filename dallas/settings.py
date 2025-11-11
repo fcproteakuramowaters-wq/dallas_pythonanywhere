9a5c9a0abad7d8eb9fa3a53a938fbfb5d7bc4d06
fcproteakuramowaters-wq <fc.proteakuramowaters@gmail.com>
2025-11-11 18:17:10 +0100

    new update settings.py

--- dallas/settings.py
+++ dallas/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-cyo+n&o=+o8h$2as5w6du+&l0119-)w=2b3sg2(o^!d-@6&@5-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['https://dallas-owo3.onrender.com','localhost','127.0.0.1']
+ALLOWED_HOSTS = ['dallas-owo3.onrender.com','localhost','127.0.0.1']
 
 
 # Application definition

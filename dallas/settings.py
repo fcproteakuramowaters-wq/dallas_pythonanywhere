553d245e1e2b9a2e3a1e577863decd566400b58b
Fatukasikayode <fatukasikayode2013@excite.com>
2025-11-11 17:52:45 +0100

    Dallas Hotel update

--- dallas/settings.py
+++ dallas/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-cyo+n&o=+o8h$2as5w6du+&l0119-)w=2b3sg2(o^!d-@6&@5-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["https://dallas-owo3.onrender.com","127.0.0.1"]
 
 
 # Application definition
